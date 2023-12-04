f6e9399c66ee152bb59b162ed694ebda78f3d238
Kroenenn <gabrielps200325@gmail.com>
2023-12-04 17:49:32 -0600

    Version funcion magnitud y angulo

--- Problema2/Problema2.c
+++ Problema2/Problema2.c
@@ -27,11 +27,29 @@ float producto_punto(Punto a, Punto b) {
     return resultado;
 }
 
+float magnitud(Punto a) {
+    float resultado = sqrt(a.x * a.x + a.y * a.y);
+    printf("Magnitud: %f\n", resultado);
+    return resultado;
+}
+
+float angulo_entre_vectores(Punto a, Punto b) {
+    float punto = producto_punto(a, b);
+    float magnitud_a = magnitud(a);
+    float magnitud_b = magnitud(b);
+    float angulo = acos(punto / (magnitud_a * magnitud_b));
+    angulo = angulo * (180.0 / 3.14159265358979323846);
+    printf("Ángulo entre vectores: %f grados\n", angulo);
+    return angulo;
+}
+
 int main() {
     Punto p1 = {1, 2};
     Punto p2 = {4, 6};
     Punto vector = crear_vector(p1, p2);
     float cruz = producto_cruz(p1, p2);
     float punto = producto_punto(p1, p2);
+    float magn = magnitud(vector);
+    float angulo = angulo_entre_vectores(p1, p2);
     return 0;
 }
