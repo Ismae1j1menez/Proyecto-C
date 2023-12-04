e0877b1f3b8de375766517f261509441185a181a
Kroenenn <gabrielps200325@gmail.com>
2023-12-04 12:46:03 -0600

    Funcion producto cruz y producto punto

--- Problema2/Problema2.c
+++ Problema2/Problema2.c
@@ -15,9 +15,23 @@ Punto crear_vector(Punto a, Punto b) {
     return vector;
 }
 
+float producto_cruz(Punto a, Punto b) {
+    float resultado = a.x * b.y - a.y * b.x;
+    printf("Producto cruz: %f\n", resultado);
+    return resultado;
+}
+
+float producto_punto(Punto a, Punto b) {
+    float resultado = a.x * b.x + a.y * b.y;
+    printf("Producto punto: %f\n", resultado);
+    return resultado;
+}
+
 int main() {
     Punto p1 = {1, 2};
     Punto p2 = {4, 6};
     Punto vector = crear_vector(p1, p2);
+    float cruz = producto_cruz(p1, p2);
+    float punto = producto_punto(p1, p2);
     return 0;
 }
