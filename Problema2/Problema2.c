1dc26e3a1760229d914c96ee8ed6354b32f27d6a
Kroenenn <gabrielps200325@gmail.com>
2023-12-04 18:59:03 -0600

    Funcion es_convexo

--- Problema2/Problema2.c
+++ Problema2/Problema2.c
@@ -43,6 +43,23 @@ float angulo_entre_vectores(Punto a, Punto b) {
     return angulo;
 }
 
+int es_convexo(Punto vertices[], int n) {
+    if (n < 3) return 0;
+
+    Punto v1 = crear_vector(vertices[0], vertices[1]);
+    Punto v2 = crear_vector(vertices[1], vertices[2]);
+    float signo = producto_cruz(v1, v2);
+
+    for (int i = 1; i < n; i++) {
+        v1 = crear_vector(vertices[i], vertices[(i + 1) % n]);
+        v2 = crear_vector(vertices[(i + 1) % n], vertices[(i + 2) % n]);
+        float prod_cruz = producto_cruz(v1, v2);
+
+        if (signo * prod_cruz < 0) return 0;
+    }
+    return 1;
+}
+
 int main() {
     Punto p1 = {1, 2};
     Punto p2 = {4, 6};
@@ -51,5 +68,14 @@ int main() {
     float punto = producto_punto(p1, p2);
     float magn = magnitud(vector);
     float angulo = angulo_entre_vectores(p1, p2);
+    Punto vertices[] = {{0, 0}, {1, 1}, {1, 0}};
+    int esConvexo = es_convexo(vertices, 3);
+    const char* mensaje;
+    if (esConvexo) {
+        mensaje = "convexo";
+    } else {
+        mensaje = "no convexo";
+    }
+    printf("El polígono es %s\n", mensaje);
     return 0;
 }
