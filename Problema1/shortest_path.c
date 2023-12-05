99eeba4c3392cb3dec192d7464ea05d8f192a284
Ismael <IJime31@gmail.com>
2023-12-05 09:48:44 -0600

    Arreglo de comentarios

--- Problema1/shortest_path.c
+++ Problema1/shortest_path.c
@@ -3,27 +3,27 @@
 #include <limits.h>
 #include <string.h>
 
-// Esta macro es puesta aqui para que el el tamaño maximo de la 
+// Esta macro es puesta aquí para que el tamaño máximo de la 
 // matriz sea conocida en la compilación y no se tenga que usar una
-// memoria dinamica para establecer la matriz
+// memoria dinámica para establecer la matriz
 #define MAX_CITIES 100
 int distances[MAX_CITIES][MAX_CITIES];
 
 // Inicialización de variables globales con valores definidos
-// variables tipo enteros y arreglos usados mas adelante
+// variables tipo enteros y arreglos usados más adelante
 int best_distance = INT_MAX;
 int best_path[MAX_CITIES];
 int current_path[MAX_CITIES];
 
-// Función quue itera con puros ciclos for todos los caminos posibles para llegar a un destino
+// Función que itera con puros ciclos for todos los caminos posibles para llegar a un destino
 // Esta función es llamada en input_num_len_cities_matrix()
 void find_shortest_path(int origin, int destination, int num_cities, int path_length, int path_index) {
     current_path[path_index] = origin;
     // Revisar si el destino sea == a origen siempre y cuando 
-    // el indice sea mayor a 0, porque si es cero es la misma 
+    // el índice sea mayor a 0, porque si es cero es la misma 
     // ciudad
     if (origin == destination && path_index > 0) {
-        // Primera iteración compara con INT_MAX, las demas
+        // Primera iteración compara con INT_MAX, las demás
         // compara con la variable actualizada
         if (path_length < best_distance) {
             // Asigna el valor si realmente es la menor longitud
@@ -35,20 +35,20 @@ void find_shortest_path(int origin, int destination, int num_cities, int path_le
         return;
     }
     
-    // Itera sobre el numero de ciudades
+    // Itera sobre el número de ciudades
     for (int i = 0; i < num_cities; i++) {
         // Revisa que haya una conexión siempre y cuando no sea -1 o cero porque no hay camino
-        // Ademas se asume que no pueden haber caminos con distancias negativas para no complicar 
+        // Además se asume que no puede haber caminos con distancias negativas para no complicar 
         // demasiado la lógica
         if (distances[origin][i] != -1 && distances[origin][i] != 0) {
             int cycle = 0;
             for (int j = 0; j <= path_index; j++) {
                 // Esta condición permite identificar bucles infinitos que no llevan a nada
-                // Si de A voy a B y de B a A esto crea un bucle infinito, pero facilmente se 
-                // evita con la condicipon i = j, porque al ser iguales significa que paso por 
+                // Si de A voy a B y de B a A esto crea un bucle infinito, pero fácilmente sé 
+                // evita con la condición i = j, porque al ser iguales significa que paso por 
                 // ahí una vez
                 if (current_path[j] == i) {
-                    // Se detecta el ciclo y se usa break para terminar la interación
+                    // Se detecta el ciclo y se usa break para terminar la iteración
                     cycle = 1;
                     break;
                 }
@@ -64,14 +64,14 @@ void find_shortest_path(int origin, int destination, int num_cities, int path_le
 // Esta función es llamada en input_num_len_cities_matrix()
 void print_best_path(int destination) {
     // Compara el valor de best_distance, siempre y cuando 
-    // INT_MAX sea mayoe signifca que en alguna otra función 
-    // best_distance cambio
+    // INT_MAX sea menor significa que en alguna otra función 
+    // best_distance fue actualizado
     if (best_distance < INT_MAX) {
         printf("La distancia más corta es: %d\n", best_distance);
         printf("La mejor ruta es: ");
         for (int i = 0; i < MAX_CITIES && best_path[i] != destination; i++) {
-            // Se aprovecha que los ASCII para pasar de A mas B solo se le suma
-            // 1, esto perimte cambiar los ASCCI facilmente 
+            // Se aprovecha que los ASCII para pasar de A más B solo se le suma
+            // 1, esto permite cambiar los ASCCI fácilmente
             printf("%c -> ", 'A' + best_path[i]);
         }
         printf("%c\n", 'A' + destination);
@@ -84,7 +84,7 @@ void print_best_path(int destination) {
 void input_num_len_cities_matrix() {
     // Falta revisar que se ingresen valores adecuados
     // Por lo menos que se salga del problema y se indique 
-    // porque se salio o no es adecuado ingresar ciertos valores
+    // el motivo por el cual termino el programa
     int num_cities;
     printf("Ingrese el número de ciudades: ");
     scanf("%d", &num_cities);
@@ -92,7 +92,7 @@ void input_num_len_cities_matrix() {
     for (int i = 0; i < num_cities; i++) {
         for (int j = 0; j < num_cities; j++) {
             // Condición de que siempre que i == j 
-            // el valores es 0, porque no hay distancia
+            // los valores es 0, porque no hay distancia
             // entre las dos ciudades
             if (i == j) {
                 distances[i][j] = 0;
@@ -118,7 +118,7 @@ void input_num_len_cities_matrix() {
         printf("%c ", 'A' + i);
         for (int j = 0; j < num_cities; j++) {
             // Valores que se imprimen en la matriz, 
-            // para observar la matriz ingresada por el
+            // para observar la matriz ingresada por él
             // usuario
             if (distances[i][j] == 0) {
                 printf("  -");
@@ -138,7 +138,7 @@ void input_num_len_cities_matrix() {
     int origin_city_index = origin_city - 'A';
     int dest_city_index = dest_city - 'A';
 
-    // Aqui se llaman las funciones hechas en el código anterior
+    // Aquí se llaman las funciones hechas en el código anterior
     find_shortest_path(origin_city_index, dest_city_index, num_cities, 0, 0);
 
     print_best_path(dest_city_index);
